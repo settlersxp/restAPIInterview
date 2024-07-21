c94c016b61cd40206b2b3a4465514fc50b2637bc
Cliseru Gabriel <settlersxp@gmail.com>
2024-07-21 15:26:16 +0200

    adding decimal restrictions for columns

--- api/models/WeatherLocation.cs
+++ api/models/WeatherLocation.cs
@@ -1,11 +1,20 @@
+using System.Net.Mime;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+
 namespace api.Models
 {
     public class WeatherLocation
     {
         public int Id { get; set; }
         public string Date { get; set; } = string.Empty;
-        public int TemperatureC { get; set; } = 0;
-        public WeatherSummaries Summary { get; set; } = WeatherSummaries.Warm;
-        public int TemperatureF { get; set; } = 0;
+
+        [Column(TypeName = "decimal(3, 2)")]
+        public decimal TemperatureC { get; set; } = 0;
+
+        public WeatherSummaries? Summary { get; set; }
+
+        [Column(TypeName = "decimal(3, 2)")]
+        public decimal TemperatureF { get; set; } = 0;
     }
 }
